1a2649cf43ce6dc4302ce5934d62cfce4b666a5c
xijniN <ninjixgames@gmail.com>
2024-07-14 19:54:21 -0300

    Added better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use rust_webserver::web::response_by_route;
 use std::{
-    fs,
+    env, fs,
     io::{Error, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn main() -> Result<(), Error> {
-    let listener = TcpListener::bind("127.0.0.1:8246")?;
+    let args = env::args().collect::<Vec<String>>();
+
+    let port = if let Some(arg) = args.get(1) {
+        arg.clone()
+    } else {
+        "8123".to_string()
+    };
+
+    let mut ip = "127.0.0.1:".to_string();
+    ip.push_str(&port);
+
+    let listener = TcpListener::bind(ip)?;
 
     for req in listener.incoming() {
-        handle_connection(req?)?;
+        let _ = handle_connection(req?);
     }
 
     Ok(())
@@ -19,7 +30,7 @@ fn handle_connection(mut req: TcpStream) -> Result<(), Error> {
     let mut buf = [0u8; 1024];
     req.read(&mut buf)?;
 
-    let res = response_by_route(&buf);
+    let res = response_by_route(&buf)?;
     let res = res.as_bytes();
     req.write(res)?;
     req.flush()?;
--- src/web.rs
+++ src/web.rs
@@ -1,3 +1,5 @@
+use std::io::{Error, ErrorKind};
+
 pub struct HttpRequest {
     pub method: String,
     pub version: String,
@@ -6,33 +8,54 @@ pub struct HttpRequest {
 }
 
 impl HttpRequest {
-    pub fn from(req: &str) -> Self {
+    pub fn from(req: &str) -> Result<Self, std::io::Error> {
         let parts = req
             .split(|c| ['\r', '\n', ' '].contains(&c))
             .filter(|txt| !txt.is_empty())
             .collect::<Vec<&str>>();
 
-        let method = parts[0].to_string();
-        let route = parts[1].to_string();
-        let version = parts[2].to_string();
-        let body = parts[3..].concat();
+        let method = parts.get(0);
+        let route = parts.get(1);
+        let version = parts.get(2);
+        let body = if parts.len() < 4 {
+            None
+        } else {
+            Some(parts[3..].concat())
+        };
 
-        HttpRequest {
+        if method.is_none() {
+            return Err(Error::new(ErrorKind::InvalidInput, "No method"));
+        }
+        let method = method.unwrap().to_string();
+        if route.is_none() {
+            return Err(Error::new(ErrorKind::InvalidInput, "No route"));
+        }
+        let route = route.unwrap().to_string();
+        if version.is_none() {
+            return Err(Error::new(ErrorKind::InvalidInput, "No version"));
+        }
+        let version = version.unwrap().to_string();
+        if body.is_none() {
+            return Err(Error::new(ErrorKind::InvalidInput, "No version"));
+        }
+        let body = body.unwrap();
+
+        Ok(HttpRequest {
             method,
             route,
             version,
             body,
-        }
+        })
     }
 }
 
-pub fn response_by_route(buf: &[u8]) -> String {
+pub fn response_by_route(buf: &[u8]) -> Result<String, std::io::Error> {
     use std::fs;
     use std::path::Path;
 
     let req = String::from_utf8_lossy(buf).to_string();
     let req = req.as_str();
-    let req = HttpRequest::from(req);
+    let req = HttpRequest::from(req)?;
     let default_response = "HTTP/1.1 200 OK\r\n\r\nInvalid route";
     let mut route_path = "./html".to_string();
     route_path.push_str(&req.route);
@@ -47,8 +70,8 @@ pub fn response_by_route(buf: &[u8]) -> String {
         let html = fs::read_to_string(route_path).unwrap();
         let mut res = "HTTP/1.1 200 OK\r\n\r\n".to_string();
         res.push_str(&html);
-        res
+        Ok(res)
     } else {
-        default_response.to_string()
+        Ok(default_response.to_string())
     }
 }
